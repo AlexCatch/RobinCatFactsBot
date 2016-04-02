25784ba6b57abd28fbcd91eb5292d3d1a571581e
AlexCatch <AlexCatch@users.noreply.github.com>
2016-04-02 20:08:35 +0100

    Implemented a regex to fit multiple statements.

--- bot.js
+++ bot.js
@@ -13,33 +13,24 @@
    'use strict';
     var websocket;
     var wsUri = r.config.robin_websocket_url;
-    var output;
 
     websocket = new WebSocket(wsUri);
-    websocket.onopen = function(evt) {onOpen(evt);};
-    websocket.onclose = function(evt) {onClose(evt);};
     websocket.onmessage = function(evt) {onMessage(evt); };
-    websocket.onerror = function(evt) {onError(evt);};
-    //set interval for start message
-    setInterval(function(){ sendMessage("I am a bot, send me a message with @catfact for a cat fact! [https://github.com/AlexCatch/RobinCatFactsBot]");}, 300000);
-
-    function onOpen(evt)
-    {
-        console.log('connected' + evt.data);
-    }
-
-    function onClose(evt)
-    {
-        writeToScreen("DISCONNECTED" + evt.data);
-    }
+    
+    setTimeout(function() {
+        sendMessage("I am a bot, send me a message with either !catfact, @catfact or @catfacts for a cat fact! [https://github.com/AlexCatch/RobinCatFactsBot]");
+    }, 2000);
+    
+    setInterval(function(){ sendMessage("I am a bot, send me a message with either !catfact, @catfact or @catfacts for a cat fact! [https://github.com/AlexCatch/RobinCatFactsBot]");}, 300000);
 
     function onMessage(evt)
     {
         //recieved a message
         var jsonObject = JSON.parse(evt.data);
         if (jsonObject.type == "chat") {
+            var regex = /^(!|@)cat\s?(bot|facts?)/i;
             //recieved message is a chat message, compare contents
-            if (jsonObject.payload.body == "@catfact") {
+            if (regex.test(jsonObject.payload.body.toLowerCase())) {
                 console.log("make said request");
                 //message was direcxted to us
                 makeRequest(function(fact) {
@@ -47,20 +38,9 @@
                     sendMessage(fact);
                 });
             }
-        }else if (jsonObject.type == "join" && jsonObject.payload.user == "CatFactRobinBot") {
-            //now send messsage
-            //wait 2 seconds before sending welcome message to ensure connection
-            console.log("Waiting 5 seconds");
-            setTimeout(function() {
-                sendMessage("I am a bot, send me a message with @catfact for a cat fact!");
-            }, 2000);
         }
     }
-
-    function onError(evt)
-    {
-        writeToScreen("error occured" + evt.data);
-    }
+    
     function sendMessage(message) {
         $("#robinSendMessage > input[type='text']").val("");
         $("#robinSendMessage > input[type='text']").val(message);
@@ -86,7 +66,4 @@
             }
         });
     }
-
-
-
 })();
